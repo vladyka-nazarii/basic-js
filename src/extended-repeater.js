7905a437490d46b8660a714269e47fd71017d23b
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-10-16 21:29:23 +0300

    feat: resolved extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  let additional = '';
+  if (typeof options.addition != 'undefined') {
+    additional = options.addition;
+    if (typeof options.additionRepeatTimes != 'undefined') {
+      for (let i = 1; i < options.additionRepeatTimes; i++) {
+        if (options.additionSeparator) {
+          additional += options.additionSeparator + options.addition;
+        } else {
+          additional += '|' + options.addition;
+        }
+      }
+    }
+  }
+  let part = (str + additional);
+  let result = part;
+  if (options.repeatTimes) {
+    for (let i = 1; i < options.repeatTimes; i++) {
+      if (options.separator != undefined) {
+        result += options.separator + part;
+      } else {
+        result += '+' + part;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
