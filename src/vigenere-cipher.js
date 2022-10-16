025f6f3f84998327c12c9d08d8aae3fa4b8eaa58
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-10-16 23:26:31 +0300

    feat: resolve vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (typeof message === "undefined" || typeof key === "undefined") {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageNum = message.toUpperCase().split('')
+    .map(e => (e.charCodeAt() > 64 && e.charCodeAt() < 91) ? e.charCodeAt() : e);
+
+    let messageLength = 0;
+    messageNum.forEach(e => {
+      if (typeof e === 'number') messageLength++
+    });
+
+    let keyNum = key.repeat(Math.ceil(messageLength / key.length))
+    .slice(0, messageLength).toUpperCase().split('')
+    .map(e => e.charCodeAt());
+    messageNum.forEach((e, i) => {
+      if (typeof e !== 'number') {
+        keyNum.splice(i, 0, '');
+      }
+    })
+
+    let result = messageNum.map((e, i) => {
+      if (typeof e === 'number') {
+        if (e + keyNum[i] - 65 > 90) {
+          return String.fromCharCode(e + keyNum[i] - 91);
+        } else {
+        return String.fromCharCode(e + keyNum[i] - 65);
+        }
+      } else return e
+    });
+    if (this.type == false) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    }
+  }
+
+  decrypt(message, key) {
+    if (typeof message === "undefined" || typeof key === "undefined") {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageNum = message.toUpperCase().split('')
+    .map(e => (e.charCodeAt() > 64 && e.charCodeAt() < 91) ? e.charCodeAt() : e);
+
+    let messageLength = 0;
+    messageNum.forEach(e => {
+      if (typeof e === 'number') messageLength++
+    });
+
+    let keyNum = key.repeat(Math.ceil(messageLength / key.length))
+    .slice(0, messageLength).toUpperCase().split('')
+    .map(e => e.charCodeAt());
+    messageNum.forEach((e, i) => {
+      if (typeof e !== 'number') {
+        keyNum.splice(i, 0, '');
+      }
+    })
+
+    let result = messageNum.map((e, i) => {
+      if (typeof e === 'number') {
+        if (e - keyNum[i] < 0) {
+          return String.fromCharCode(91 + e - keyNum[i]);
+        } else {
+        return String.fromCharCode(e - keyNum[i] + 65);
+        }
+      } else return e;
+    });
+    if (this.type == false) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    }
   }
 }
 
