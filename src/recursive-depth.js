e403dcaaf80d7b1e77227342339836b9dbbd3437
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-10-17 21:06:25 +0300

    feat: resolved: recursive

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class DepthCalculator {
   calculateDepth(arr) {
-    return Array.isArray(arr) ? 1 + Math.max(0, ...arr.map((new DepthCalculator).calculateDepth)) : 0;
+    return Array.isArray(arr) ? 1 + Math.max(0, ...arr.map(e => this.calculateDepth(e))) : 0;
   }
 }
 
